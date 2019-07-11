b0e8f8624c1f9335fabb0c91198829a41ab7d2f6
fengjiahui <fengjiahui@kingsoft.com>
2019-07-11 18:08:45 +0800

    board init

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,25 @@
+import edu.princeton.cs.algs4.StdOut;
+
 public class Board {
+
+    private final int n;
+    private int[][] square;
+
     // create a board from an n-by-n array of tiles
     // where tiles[row][col] = tile at (row, col)
-    public Board(int[][] tiles) {
+    // public Board(int[][] tiles) {
+    public Board() {
+        int[][] tiles = {{2, 2}, {2, 1}, {3, 2}, {3, 1}, {1, 2}, {3, 3}, {2, 3}, {1, 3}, {1, 1}};
+
+        n = tiles.length;
+
+        square = new int[n][n];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                square[i][j] = tiles[i][j];
+            }
+        }
 
     }
 
@@ -11,14 +29,38 @@ public class Board {
 
     // board dimension n (可以是二维、三维、甚至多维？)
     public int dimension() {
+        return n;
     }
 
     // number of tiles out of place
     public int hamming() {
+        int outOfPlaceNum = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (square[i][j] != i * n + j) {
+                    outOfPlaceNum++;
+                }
+            }
+        }
+
+        return outOfPlaceNum;
     }
 
     // sum of Manhattan distance between tiles and goal
     public int manhattan() {
+
+        int[][] distances = new int[n][n];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int current = square[i][j];
+                int x = current % n;
+                int y = (current - x) / n;
+                distances[i][j] = Math.abs(x - i) + Math.abs(y - i);
+                StdOut.println("distance: " + distances[i][j]);
+            }
+        }
     }
 
     // is this board the goal board?
@@ -40,6 +82,8 @@ public class Board {
 
     // unit testing
     public static void main(String[] args) {
+        Board board = new Board();
+        board.manhattan();
     }
 
 }
